80eaac88734c31457d17358d3885594ae47c2e5f
Eddie Mieilă <eddie@Eddies-MacBook-Pro.local>
2023-10-27 14:04:25 +0300

    Integrare Recombee - Partea a II-a

--- recombee-sac1.py
+++ recombee-sac1.py
@@ -2,13 +2,20 @@ import csv
 from recombee_api_client.api_client import RecombeeClient, Region
 from recombee_api_client.api_requests import *
 
+def printRecommandations(user, rec):
+    print('Recommandations for user ' + user)
+    for r in rec["recomms"]:
+        print("\t" + r["id"])
+    print("\n")
+
 client = RecombeeClient('sac-upb-dev', 'x6p1TFmPseNHjO26HWoq1td8v3VYeAes1Un5dlq7luZunoLQOzfcCYEVvyviiC9B', region=Region.EU_WEST)
 
-inFile = 'netflix_titles.csv'
+inFileMovies = 'netflix_titles_medium.csv'
+inFileUsers = 'users.csv'
 
-# 2. Alegere și prelucrare dataset
+# I.2. Alegere și prelucrare dataset
 # Dataset: https://www.kaggle.com/datasets/shivamb/netflix-shows/
-with open(inFile, 'r') as file:
+with open(inFileMovies, 'r') as file:
     reader = csv.reader(file, delimiter=",")
     fields = next(reader) # Read the header row
     entries = {}
@@ -23,22 +30,100 @@ with open(inFile, 'r') as file:
             currShow[fields[colIndex]] = propValue
         
         entries[currId] = currShow
-    print("Read all the data from '" + inFile + "'" )
+    print("Read all the data from '" + inFileMovies + "'" )
     
-    # 3. Implementare cod upload id-uri de produse 
+    # # I.3. Implementare cod upload id-uri de produse 
+    requests = []
     for itemId, _ in entries.items():
-        client.send(AddItem(itemId))
+        r = AddItem(itemId)
+        # Delete pentru resetare DB
+        # r = DeleteItem(itemId)
+        requests.append(r)
+    client.send(Batch(requests))
     print("Sent all ids to Recombee DB")
 
-    # 4. Implementare cod salvare proprietăți produse
+    # # I.4. Implementare cod salvare proprietăți produse
+    requests = []
     for propName in fields:
-        client.send(AddItemProperty(propName, 'string'))
+        r = AddItemProperty(propName, 'string')
+        requests.append(r)
+    client.send(Batch(requests))
     print("Added all proprieties names to Recombee DB")
     
-    #  5. Implementare cod salvare valori proprietăți produse
+    # # I.5. Implementare cod salvare valori proprietăți produse
     for itemId, value in entries.items():
-        client.send(SetItemValues(itemId, value, cascade_create = True))
+        r = SetItemValues(itemId, value, cascade_create = True)
+        requests.append(r)
+    client.send(Batch(requests))
     print("Added all proprieties values to Recombee DB")
     
 
+with open(inFileUsers, 'r',  encoding='utf-8-sig') as file:
+    reader = csv.reader(file, delimiter=",")
+    userFields = next(reader) # Read the header row
+    users = {}
 
+    for row in reader:
+        currId = 0
+        currUser = {}
+
+        for colIndex, propValue in enumerate(row):
+            if (colIndex == 0):
+                currId = propValue
+            currUser[userFields[colIndex]] = propValue
+        
+        users[currId] = currUser
+    print("Read all the data from '" + inFileUsers + "'" )
+    
+    # # II.1. Adauga id-uri client
+    requests = []
+    for userId, _ in users.items():
+        r = AddUser(userId)
+        # Delete pentru resetare DB
+        # r = DeleteUser(userId)
+        requests.append(r)
+    client.send(Batch(requests))
+    print("Sent all user ids to Recombee DB")
+
+    # # II.2. Adauga interactiuni user-movie
+    interactions = []
+    interactions.append(AddDetailView("u001", "s1"))
+    interactions.append(AddDetailView("u001", "s13"))
+    interactions.append(AddDetailView("u001", "s32"))
+    interactions.append(AddDetailView("u001", "s23"))
+
+    interactions.append(AddDetailView("u002", "s73"))
+    interactions.append(AddDetailView("u002", "s19"))
+
+    interactions.append(AddDetailView("u003", "s44"))
+    interactions.append(AddDetailView("u003", "s24"))
+    interactions.append(AddDetailView("u003", "s32"))
+    interactions.append(AddDetailView("u003", "s72"))
+
+    interactions.append(AddDetailView("u004", "s92"))
+    interactions.append(AddDetailView("u004", "s94"))
+    interactions.append(AddDetailView("u004", "s21"))
+
+    interactions.append(AddDetailView("u005", "s32"))
+    interactions.append(AddDetailView("u005", "s2"))
+
+    interactions.append(AddDetailView("u006", "s7"))
+    interactions.append(AddDetailView("u006", "s6"))
+    interactions.append(AddDetailView("u006", "s9"))
+    
+    interactions.append(AddDetailView("u007", "s36"))
+    
+    interactions.append(AddDetailView("u008", "s64"))
+    
+    interactions.append(AddDetailView("u009", "s88"))
+    
+    interactions.append(AddDetailView("u010", "s26"))
+
+    client.send(Batch(interactions))
+
+
+    # # II.3. Recomanda cate 5 id-uri de filme/seriale pentru fiecare utilizator
+    for u in users:
+        r = client.send(RecommendItemsToUser(u, 5))
+        printRecommandations(u, r)
+        
